774eef3e89840d8e9b158a10f022d27cfa15b6e3
amoslee2026 <amoslee2026@users.noreply.github.com>
2026-08-05 23:08:46 +0800

    [checkpoint] 2026-08-05 23:08:46 (UTC+8)

--- utils/token_compare.py
+++ utils/token_compare.py
@@ -41,8 +41,6 @@ import shutil
 import subprocess
 import sys
 import time
-import urllib.error
-import urllib.request
 from datetime import datetime, timedelta, timezone
 from pathlib import Path
 
